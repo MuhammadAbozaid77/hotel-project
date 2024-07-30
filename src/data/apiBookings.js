83d188bb7dca8bc7a3bb3e654e63dd88879861b0
MuhammadAbozaid77 <muhammadaozaid77@gmail.com>
2024-07-30 10:20:55 +0300

    hh

--- src/data/apiBookings.js
+++ src/data/apiBookings.js
@@ -1,27 +1,42 @@
-import {
-  collection,
-  getDocs,
-  addDoc,
-  deleteDoc,
-  updateDoc,
-  doc,
-} from "firebase/firestore";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
-import { db, firebaseStorage } from "./firebaseConfig";
+// import {
+//   collection,
+//   getDocs,
+//   addDoc,
+//   deleteDoc,
+//   updateDoc,
+//   doc,
+// } from "firebase/firestore";
+// import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+// import { db, firebaseStorage } from "./firebaseConfig";
+
+// /* -------------------------- Bookings -------------------------- */
+// const bookingsCollection = collection(db, "bookings");
+// /* ----------------------- Get Bookings ----------------------- */
+// export async function getBookings() {
+//   try {
+//     const data = await getDocs(bookingsCollection);
+//     const filteredData = data.docs?.map((doc, index) => ({
+//       ...doc?.data(),
+//       id: doc?.id,
+//     }));
+//     console.log(filteredData);
+//     return filteredData || [];
+//   } catch (error) {
+//     return error;
+//   }
+// }
 
 /* -------------------------- Bookings -------------------------- */
-const bookingsCollection = collection(db, "bookings");
-/* ----------------------- Get Bookings ----------------------- */
+
+// bookings
+import supabase, { supabaseUrl } from "./supabase";
+
 export async function getBookings() {
-  try {
-    const data = await getDocs(bookingsCollection);
-    const filteredData = data.docs?.map((doc, index) => ({
-      ...doc?.data(),
-      id: doc?.id,
-    }));
-    console.log(filteredData);
-    return filteredData || [];
-  } catch (error) {
-    return error;
+  const { data, error } = await supabase.from("bookings").select("*");
+
+  if (error) {
+    throw new Error("Bookings could not be loaded");
   }
+
+  return data;
 }
